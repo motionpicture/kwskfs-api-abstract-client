3c4099b3a34f58e71d9edfd27ded08bb229c0254
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-05-07 06:33:50 +0000

    # KWSKFS-12 ### Added - イベント販売情報検索サービスを追加。 - イベント販売情報の在庫状況更新サービスを追加。

--- src/service/event.ts
+++ src/service/event.ts
@@ -1,5 +1,5 @@
 import * as factory from '@motionpicture/kwskfs-factory';
-import { OK } from 'http-status';
+import { NO_CONTENT, OK } from 'http-status';
 
 import { Service } from '../service';
 
@@ -9,11 +9,18 @@ export type IEvent<T> =
     factory.event.IEvent;
 
 /**
- * event service
+ * イベントタイプによる販売情報インターフェース
+ */
+export declare type IOfferByEventType<T> =
+    T extends factory.eventType.FoodEvent ? factory.organization.restaurant.IOrganization :
+    any;
+
+/**
+ * イベントサービス
  */
 export class EventService extends Service {
     /**
-     * 組織検索
+     * イベント検索
      */
     // tslint:disable-next-line:no-single-line-block-comment
     /* istanbul ignore next */
@@ -32,6 +39,70 @@ export class EventService extends Service {
             method: 'GET',
             qs: params,
             expectedStatusCodes: [OK]
+        });
+    }
+
+    /**
+     * 特定イベントの販売情報を検索する
+     */
+    // tslint:disable-next-line:no-single-line-block-comment
+    /* istanbul ignore next */
+    public async searchOffers<T extends factory.eventType>(params: {
+        /**
+         * イベントタイプ
+         */
+        eventType: T;
+        /**
+         * イベント識別子
+         */
+        eventIdentifier: string;
+    }): Promise<IOfferByEventType<T>[]> {
+        return this.fetch({
+            uri: `/events/${params.eventType}/${params.eventIdentifier}/offers`,
+            method: 'GET',
+            qs: params,
+            expectedStatusCodes: [OK]
+        });
+    }
+
+    /**
+     * 特定イベントの特定販売情報に対して、在庫状況を更新する
+     * 管理者権限が必要です。
+     */
+    // tslint:disable-next-line:no-single-line-block-comment
+    /* istanbul ignore next */
+    public async changeMenuItemOfferAvailability(params: {
+        /**
+         * イベントタイプ
+         */
+        eventType: factory.eventType;
+        /**
+         * イベント識別子
+         */
+        eventIdentifier: string;
+        /**
+         * メニューアイテムを提供する組織ID
+         */
+        organizationId: string;
+        /**
+         * メニューアイテム識別子
+         */
+        menuItemIdentifier: string;
+        /**
+         * オファー識別子
+         */
+        offerIdentifier: string;
+        /**
+         * 在庫状況
+         */
+        availability: factory.itemAvailability;
+    }): Promise<void> {
+        return this.fetch({
+            // tslint:disable-next-line:max-line-length
+            uri: `/events/${params.eventType}/${params.eventIdentifier}/offers/${params.organizationId}/menuItem/${params.menuItemIdentifier}/${params.offerIdentifier}/availability/${params.availability}`,
+            method: 'PUT',
+            qs: params,
+            expectedStatusCodes: [NO_CONTENT]
         });
     }
 }
